d7f66f5677ebc70c9907c619aac90974851edf1e
katherineboliek <katherineboliek@gmail.com>
2016-04-14 10:41:58 -0400

    Major changes, switched to a different chart.

--- app.js
+++ app.js
@@ -1,95 +1,92 @@
-var svg = d3.select(".container")
-	.append("svg")
-    .attr('width', '100%')
-    .attr('height', '100%')
-	.append("g")
+	var margin = {top: 20, right: 80, bottom: 30, left: 50},
+	    width = 960 - margin.left - margin.right,
+	    height = 500 - margin.top - margin.bottom;
 
+	var svg = d3.select(".container").append("svg")
+			    .attr("width", width + margin.left + margin.right)
+			    .attr("height", height + margin.top + margin.bottom)
+			  .append("g")
+			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-svg.append("g")
-	.attr("class", "slices");
+	var parseDate = d3.time.format("%Y").parse;
 
-var width = 480,
-    height = 225,
-	radius = Math.min(width, height) / 2;
+	var x = d3.time.scale()
+	    .range([0, width]);
 
-var pie = d3.layout.pie()
-	.sort(null)
-	.value(function(d) {
-		return d.value;
+	var y = d3.scale.linear()
+	    .range([height, 0]);
+
+	var color = d3.scale.ordinal()
+  .domain(["All","18-29","30-49","50-64","65+"])
+  .range(["#b0d5c3", "#ff8a6d" , "#de6344", "#CE3D33", "#6F020E"]);
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left");
+
+	var line = d3.svg.line()
+	    .interpolate("basis")
+	    .x(function(d) { return x(d.year); })
+	    .y(function(d) { return y(d.percentage); });
+
+	d3.tsv("media.txt", function(error, data) {
+	  if (error) throw error;
+
+	  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; }));
+
+	  data.forEach(function(d) {
+	    d.year = parseDate(d.year);
+	  });
+
+	  var ages = color.domain().map(function(name) {
+	    return {
+	      name: name,
+	      values: data.map(function(d) {
+	        return {year: d.year, percentage: +d[name]};
+	      })
+	    };
+	  });
+
+	  x.domain(d3.extent(data, function(d) { return d.year; }));
+
+	  y.domain([
+	    d3.min(ages, function(c) { return d3.min(c.values, function(v) { return v.percentage; }); }),
+	    d3.max(ages, function(c) { return d3.max(c.values, function(v) { return v.percentage; }); })
+	  ]);
+
+	  svg.append("g")
+	      .attr("class", "x axis")
+	      .attr("transform", "translate(0," + height + ")")
+	      .call(xAxis);
+
+	  svg.append("g")
+	      .attr("class", "y axis")
+	      .call(yAxis)
+	    .append("text")
+	      .attr("transform", "rotate(-90)")
+	      .attr("y", 6)
+	      .attr("dy", ".71em")
+	      .style("text-anchor", "end")
+	      .text("Percent of users");
+
+	  var age = svg.selectAll(".age")
+	      .data(ages)
+	    .enter().append("g")
+	      .attr("class", "age");
+
+	  age.append("path")
+	      .attr("class", "line")
+	      .attr("d", function(d) { return line(d.values); })
+	      .style("stroke", function(d) { return color(d.name); });
+
+	  age.append("text")
+	      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+	      .attr("transform", function(d) { return "translate(" + x(d.value.year) + "," + y(d.value.percentage) + ")"; })
+	      .attr("x", 3)
+	      .attr("dy", ".35em")
+	      .text(function(d) { return d.name; });
 	});
-
-var arc = d3.svg.arc()
-	.outerRadius(radius * 0.8)
-	.innerRadius(radius * 0.5);
-
-var outerArc = d3.svg.arc()
-	.innerRadius(radius * 0.9)
-	.outerRadius(radius * 0.9);
-
-svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-var key = function(d){ return d.data.label; };
-
-var color = d3.scale.ordinal()
-	.domain(["using", "not using"])
-	.range(["#DE6344", "#FF8A6D"]);
-
-function randomData (){
-	var labels = color.domain();
-	return labels.map(function(label){
-		return { label: label, value: Math.random() }
-	});
-}
-
-// var year = 2005;
-// ​
-// function doStep() {
-//     setTimeout(yearStep, 2000);
-// }
-// ​
-// function yearStep() {
-// ​
-//     doTransition(year);
-//     year++;
-//     if (year >= 2015) return;
-//     doStep();
-// ​
-// }
-//
-// d3.select(".play")
-// 	.on("click", function(){
-// 		doStep();
-// 	});
-
-change(randomData());
-
-d3.select(".play")
-	.on("click", function(){
-		change(randomData());
-	});
-
-
-function change(data) {
-
-	var slice = svg.select(".slices").selectAll("path.slice")
-		.data(pie(data), key);
-
-	slice.enter()
-		.insert("path")
-		.style("fill", function(d) { return color(d.data.label); })
-		.attr("class", "slice");
-
-	slice
-		.transition().duration(1000)
-		.attrTween("d", function(d) {
-			this._current = this._current || d;
-			var interpolate = d3.interpolate(this._current, d);
-			this._current = interpolate(0);
-			return function(t) {
-				return arc(interpolate(t));
-			};
-		})
-
-	slice.exit()
-		.remove();
-};
